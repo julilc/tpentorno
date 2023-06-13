567b841a502b069e5b0e6fbed5a6595daaeb3a5e
julilc <julietalopezceratto@gmail.com>
2023-06-13 19:41:51 -0300

    se cambia archivo generar.sh

--- generar.sh
+++ generar.sh
@@ -1,10 +1,12 @@
 #! usr/bin/bash
 
-if [ $# -eq 0 ]; then
+if [ $# -gt 0 ]; then
 	for argumentos in $#; do
 		nombre="$(sort -R nombres.csv |  tail -n 1 | cut -d "," -f "1" | cut -d " " -f "1")"
-		while  [-e $nombre "/imagenes"]; do
+		control=$(ls imagenes | grep $nombre)
+		while  [$control -eq 0]; do
 			$nombre=$(sort -R nombres.csv |  tail -n 1 | cut -d "," -f "1" | cut -d " " -f "1")
+			$control=$(ls imagenes | grep $nombre)
 		done
 	wget https://source.unsplash.com/random/900%C3%97700/?person -O $nombre
 	mv $nombre imagenes
