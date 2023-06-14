04747b121aade640fb0729001169e794aa15bd7f
julilc <julietalopezceratto@gmail.com>
2023-06-14 20:14:30 -0300

    corrigo iteracion por el numero del argumento pasado en vez de la cantidad de argumentos pasado y tambien el archivo suma de verificacion

--- generar.sh
+++ generar.sh
@@ -1,22 +1,22 @@
-#! usr/bin/bash
+#!usr/bin/bash
 
 if [ $# -gt 0 ]; then
-	mkdir imagenes
-	for argumentos in $#; do
-		nombre="$(sort -R nombres.csv |  tail -n 1 | cut -d "," -f "1" | cut -d " " -f "1")"
-		control=$(ls imagenes | grep "^$nombre$")
-		while  [$control -eq 0]; do
-			$nombre=$(sort -R nombres.csv |  tail -n 1 | cut -d "," -f "1" | cut -d " " -f "1")
+	mkdir -p imagenes
+	i=0
+	until [[ $i == $@ ]]; do
+		nombre=$(sort -R nombres.csv |  tail -n 1 | cut -d "," -f "1" | cut -d " " -f "1")
+		control=$(ls imagenes | grep "^$nombre$"; echo $?)
+		while [[ $control -eq 0 ]]; do
+			nombre=$(sort -R nombres.csv |  tail -n 1 | cut -d "," -f "1" | cut -d " " -f "1")
 			$control=$(ls imagenes | grep $nombre)
 		done
-	wget https://source.unsplash.com/random/900%C3%97700/?person -O $nombre
-	mv $nombre imagenes
+		wget https://source.unsplash.com/random/900%C3%97700/?person -O $nombre
+		mv $nombre imagenes
+		sleep 1s
+		((i++))
 	done
-	for imagen in imagenes/*; do
-		sha256sum "$imagen" >> sumimagenes
-	done
-	mv sumimagenes imagenes -i
 	zip -r imagenes.zip imagenes
+	echo $(sha256sum imagenes.zip)>sumimagenes
 	rm -r imagenes
 exit 0
 
