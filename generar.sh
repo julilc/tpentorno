c068087fdc97b238980d16ffdc7fc21ed7889667
julilc <julietalopezceratto@gmail.com>
2023-06-26 11:40:36 -0300

    se agrega modificacion para mayq en nombres

--- generar.sh
+++ generar.sh
@@ -5,14 +5,15 @@ if [ $# -gt 0 ]; then
 	mkdir -p imagenes
 	i=0
 	until [[ $i == $@ ]]; do
-		nombre=$(sort -R nombres.csv |  tail -n 1 | cut -d "," -f "1" | cut -d " " -f "1")
-		control=$(ls imagenes | grep "^$nombre$"; echo $?)
-		while [[ $control -eq 0 ]]; do
+		NOMBRE=$(sort -R nombres.csv |  tail -n 1 | cut -d "," -f "1" | cut -d " " -f "1")
+		CONTROL=$(ls imagenes | grep "^$NOMBRE$"; echo $?)
+		while [[ $CONTROL -eq 0 ]]; do
 			nombre=$(sort -R nombres.csv |  tail -n 1 | cut -d "," -f "1" | cut -d " " -f "1")
-			$control=$(ls imagenes | grep $nombre)
+			$CONTROL=$(ls imagenes | grep $NOMBRE)
 		done
-		wget https://source.unsplash.com/random/900%C3%97700/?person -O $nombre
-		mv $nombre imagenes
+		NOMBREMAYQ="${NOMBRE^}"
+		wget https://source.unsplash.com/random/900%C3%97700/?person -O $NOMBREMAYQ
+		mv $NOMBREMAYQ imagenes
 		sleep 1s
 		((i++))
 	done
