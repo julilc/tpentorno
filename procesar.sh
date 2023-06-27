1229f86b8fd735a214db2452820bb349e8e2ff09
julilc <julietalopezceratto@gmail.com>
2023-06-27 12:36:01 -0300

    agrego programa procesar.sh

---
+++ procesar.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+cd imagenes
+echo "procesando imagenes..."
+sleep 3s
+for IMAGEN in *; do
+    if [[ "$IMAGEN" =~ ^[A-Z][a-z]+$ ]]; then
+        convert "$IMAGEN" -gravity center -resize 512x512+0+0 \-extent 512x512 "$IMAGEN"
+    fi
+done
+
+echo "imagenes procesadas con exito"
+exit 0
+else
+	echo "el directorio no existe"
+exit 1
+fi
+cd ..
+
+
