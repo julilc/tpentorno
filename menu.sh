830dd32659357ff65d9b611ff9665bb63e64ad47
julietalc <jullietalopezceratto@gmail.com>
2023-07-04 17:33:26 -0300

    cambio menu

--- menu.sh
+++ menu.sh
@@ -5,17 +5,17 @@ opcion=0
 until [[ $opcion =~ [1-6] ]]; do
 	echo "(1)Generar imagenes. (2)Descargar imagenes. (3)Descomprimir imagenes. (4) Procesar imagenes. (5)Comprimir imagenes. (6)Salir."
 
-	read -p "ingrese la opcion deseada" opcion
+	read -p "ingrese la opcion deseada: " opcion
 
 	case $opcion in
   		1)
-			read -p "ingrese el numero de imagenes a generar" numero
+			read -p "ingrese el numero de imagenes a generar: " numero
 			bash generar.sh $numero
 			;;
 		2)
 			echo "ingrese las rutas con comillas antes y al terminar los enlaces. Por ejemplo 'enlace.com'"
-			read -p "ingrese ruta del archivo " ruta_archivo
-			read -p "ingrese ruta del archivo de verificacion " ruta_sum
+			read -p "ingrese ruta del archivo: " ruta_archivo
+			read -p "ingrese ruta del archivo de verificacion: " ruta_sum
 
 			bash descargar.sh "$ruta_archivo" "$ruta_sum"
 			;;
